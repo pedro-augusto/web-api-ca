0c4e2836b20e2e07ebc072f9e6aab2d0535de4cc
pedro-augusto <pedro.canteli2001@gmail.com>
2025-12-19 09:14:11 +0000

    Implement addInteraction for must watch functionality

--- react-movies/src/components/cardIcons/addToMustWatch.jsx
+++ react-movies/src/components/cardIcons/addToMustWatch.jsx
@@ -2,13 +2,22 @@ import React, { useContext } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 import IconButton from "@mui/material/IconButton";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import {addInteraction} from "../../api/interactions-api";
+import { AuthContext } from "../../contexts/authContext";
 
 const AddToMustWatchIcon = ({ movie }) => {
   const context = useContext(MoviesContext);
+  const { userName } = useContext(AuthContext); 
 
   const handleAddToMustWatch = (e) => {
     e.preventDefault();
     context.addToMustWatch(movie);
+
+    addInteraction({
+      movieId: movie.id,
+      username: userName,
+      interactionType: "Must Watch"
+    });
   };
   
   return (
