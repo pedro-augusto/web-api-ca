0b0d18df4a61fa7b69b009b614cd5cd62ab8b99a
pedro-augusto <pedro.canteli2001@gmail.com>
2025-12-20 18:42:33 +0000

    Styling register page

--- react-movies/src/pages/registerPage.jsx
+++ react-movies/src/pages/registerPage.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { Navigate } from "react-router";
 import { AuthContext } from '../contexts/authContext';
+import {Container, Card, CardContent, Typography, TextField, Button, Box} from "@mui/material";
 
 const RegisterPage = () => {
   const context = useContext(AuthContext)
@@ -20,25 +21,63 @@ const RegisterPage = () => {
   }
 
   if (registered === true) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/user/login" />;
   }
 
   return (
-    <>
-      <h2>SignUp page</h2>
-      <p>You must register a username and password to log in. Usernames must be unique and passwords must contain a minimum of 8 characters (with at least one uppercase letter, one lowercase letter, and one symbol). </p>
-      <input value={userName} placeholder="user name" onChange={e => {
-        setUserName(e.target.value);
-      }}></input><br />
-      <input value={password} type="password" placeholder="password" onChange={e => {
-        setPassword(e.target.value);
-      }}></input><br />
-      <input value={passwordAgain} type="password" placeholder="password again" onChange={e => {
-        setPasswordAgain(e.target.value);
-      }}></input><br />
-      {/* Login web form  */}
-      <button onClick={register}>Register</button>
-    </>
+  <Container maxWidth="sm">
+        <Card sx={{
+          display: "flex",
+          justifyContent: "center",
+          mt: 10
+        }}>
+          <CardContent>
+            <Typography variant="h4" align="center" gutterBottom>
+              SignUp
+            </Typography>
+
+            <Typography
+              variant="body2"
+              align="center"
+              sx={{ mb: 3 }}
+            >
+              You must register a username and password to log in. Usernames must be unique and passwords must contain a minimum of 8 characters (with at least one uppercase letter, one lowercase letter, and one symbol). 
+            </Typography>
+
+            <TextField
+              label="Username"
+              fullWidth
+              margin="normal"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+
+            <TextField
+              label="Password"
+              type="password"
+              fullWidth
+              margin="normal"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <TextField
+              label="Password (again)"
+              type="password"
+              fullWidth
+              margin="normal"
+              onChange={(e) =>  setPasswordAgain(e.target.value)}
+            />
+
+            <Button
+              variant="contained"
+              fullWidth
+              sx={{ mt: 3 }}
+              onClick={register}
+            >
+              Register
+            </Button>
+          </CardContent>
+        </Card>
+    </Container>
   );
 };
 
